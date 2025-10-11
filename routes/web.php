13137869aa8fe1267315ec1cde83f01262216e0b
swarna38 <swarna.sutradhar.info@gamil.com>
2025-10-11 09:27:34 +0600

    for custom middleware create route resource that use auto crud 7 route and extra route create check documents publish or not

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocumentsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,12 +14,23 @@ Route::get('/dashboard', function () {
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
-})->middleware(['role:admin'])->name('admin.dashboard');
+})->middleware(['role:admin,manager'])->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// authenticate just login user for create
+Route::middleware('auth')->group(function(){
+
+    //rosource use shortcut its create auto 7 route such that edit update index
+    Route::resource('documents',[DocumentsController::class]);
+
+    //publish or not
+    Route::post('documents/{documents}/publish',[DocumentsController::class, 'publish'])->name  ('documents.publish');
+});
+
 
 require __DIR__.'/auth.php';
